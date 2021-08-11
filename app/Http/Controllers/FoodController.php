a432a58d0ca985564dbd71135ba31d3f251e363c
kousueke <k.koko.koko.kousuke@gmail.com>
2021-08-11 23:24:33 +0900

    1日のタンパク質摂取量用

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 
 class FoodController extends Controller
 {
+
+  public function index()
+  {
+    $items = Food::all();
+    return response()->json([
+      'data' => $items
+    ], 200);
+  }
+
   public function store(Request $request)
   {
     $item = Food::create($request->all());
@@ -15,4 +24,37 @@ class FoodController extends Controller
       'data' => $item
     ], 201);
   }
+
+  public function update(Request $request, Food $food)
+  {
+    $update = [
+      'name' => $request->name,
+      'protein' => $request->protein
+    ];
+    $item = Food::where('id', $food->id)->update($update);
+    if ($item) {
+      return response()->json([
+        'message' => 'Updated successfully',
+      ], 200);
+    } else {
+      return response()->json([
+        'message' => 'Not found',
+      ], 404);
+    }
+  }
+
+  public function destroy(Food $food)
+  {
+    $item = Food::where('id', $food->id)->delete();
+    if ($item) {
+      return response()->json([
+        'message' => 'Deleted successfully',
+      ], 200);
+    } else {
+      return response()->json([
+        'message' => 'Not found',
+      ], 404);
+    }
+  }
+
 }
---
+++ database/migrations/2021_08_06_083349_create_dailies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDailiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dailies', function (Blueprint $table) {
+            $table->string('id');
+            $table->double('amount');
+            $table->date('day');
+            $table->string('user_id');
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dailies');
+    }
+}
